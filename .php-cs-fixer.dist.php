bdff50f2dcf18c9b3a542a915bd3aa8b7d158da8
K.Utsunomiya <kii.utsunomiya@gmail.com>
2026-07-03 11:41:25 +0900

    Add PHP CS Fixer configuration file and simplify CI formatter step

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,14 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in([__DIR__ . '/src', __DIR__ . '/tests']);
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PER-CS' => true,
+        'trailing_comma_in_multiline' => [
+            'after_heredoc' => false,
+            'elements' => ['arguments', 'array_destructuring', 'arrays', 'match', 'parameters'],
+        ],
+    ])
+    ->setFinder($finder);
